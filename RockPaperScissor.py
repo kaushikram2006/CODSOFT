ed0498b0160cd03307641102813cb8a4801bfd9a
kaushikram2006 <kaushikarigela@gmail.com>
2025-09-09 19:22:11 +0530

    Rock-Paper-Scissors Game

---
+++ RockPaperScissor.py
@@ -0,0 +1,32 @@
+import random
+
+def play_game():
+    print("=" * 44)
+    print("ROCK - PAPER - SCISSORS".center(44))
+    print("=" * 44)
+
+    choices = ["rock", "paper", "scissors"]
+
+    user = input("Enter rock, paper or scissors\n(YOU) : ").lower()
+    bot = random.choice(choices)
+    print(f"BOT : {bot}")
+
+    if user not in choices:
+        print("Invalid choice!!")
+        return
+
+    if user == bot:
+        print("It's a Tie....")
+    elif (user == "rock" and bot == "scissor") or \
+         (user == "paper" and bot == "rock") or \
+         (user == "scissor" and bot == "paper"):
+        print("CONGRATULATIONS!! You won the game....")
+    else:
+        print("Oops!! You lost the game....")
+
+while True:
+    play_game()
+    again = input("| Would you like to play again (yes/no) | ").lower()
+    if again != "yes":
+        print("Thank You for playing the game")
+        break
